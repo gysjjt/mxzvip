740df6591049683da872dc62188f4c5323da624c
hexiong <284813030@qq.com>
2018-06-19 16:26:40 +0800

    ...

--- login.php
+++ login.php
@@ -10,52 +10,53 @@ header("Content-Type: text/html;charset=utf-8");
 include_once("curlapi.class.php");
 $curl = new curlapi();
 if($_GET['action'] == "code"){//获取验证码
-	$curl -> url = "http://vip.mikong.com/validatecode.aspx";
-	echo $curl -> get_code();
+    $curl -> url = "http://vip.mikong.com/validatecode.aspx";
+    echo $curl -> get_code();
 }else if($_GET['action'] == "login"){
-	$login = urlencode($_POST['login']);
-	$passwd = $_POST['passwd'];
-	$rand = $_POST['rand'];
-	//$params = "a=LoginIn&u=$login&p=$passwd&c=$rand&ts=0.6658900876500904&hi=";
-	$curl -> url = "http://vip.mikong.com/ajaxapp/commonajaxquery.ashx?a=LoginIn&u=$login&p=$passwd&ts=0.6658900876500904&hi=";
-	$curl -> params = '';
-	$result = $curl -> login();
-	if($result == '0' || $result == 0){
-		echo 1;
-	}else {
-		echo "账号密码或者验证码错误";
-	}
+    $login = urlencode($_POST['login']);
+    $passwd = $_POST['passwd'];
+    $rand = $_POST['rand'];
+    //$params = "a=LoginIn&u=$login&p=$passwd&c=$rand&ts=0.6658900876500904&hi=";
+    $curl -> url = "http://vip.mikong.com/ajaxapp/commonajaxquery.ashx?a=LoginIn&u=$login&p=$passwd&ts=0.6658900876500904&hi=";
+    $curl -> params = '';
+    $result = $curl -> login();
+    if($result == '0' || $result == 0){
+        echo 1;
+    }else {
+        echo "账号密码或者验证码错误";
+    }
 }else if($_GET['action'] == 'curlmember'){
-	$shopname = $_REQUEST['shopname'];
-	$data = '';
+    $shopname = "发宴美发美甲";
+    $data = '';
 
-	$_SESSION['cookies'] = 'JSESSIONID=BF5CE3D73CE44444E19BDD615E9513B3; Hm_lvt_cc903faaed69cca18f7cf0997b2e62c9=1523606941,1524721584,1524878191; Hm_lpvt_cc903faaed69cca18f7cf0997b2e62c9=1524878207';
+    $_SESSION['cookies'] = 'JSESSIONID=3CF7C937D90443FC02B803D0586F33B4; Hm_lvt_4e5bdf78b2b9fcb88736fc67709f2806=1529394734; Hm_lpvt_4e5bdf78b2b9fcb88736fc67709f2806=1529394948';
     //获取总数
-	$curl -> url = "http://vip.netxz.cn/member/index";
+    $curl -> url = "http://mry.meiruyi.vip/member/index";
     $rs = $curl -> getMembersPage();
     preg_match('/共(.*)条/isU', $rs, $totals);
     $totals = isset($totals[1])?$totals[1]:100;
-	$totals = preg_replace("/\s\n\t/","",$totals);
-	$totals = str_replace('&nbsp;','',$totals);
+    $totals = preg_replace("/\s\n\t/","",$totals);
+    $totals = str_replace('&nbsp;','',$totals);
+
     //总页数
     $pages = ceil($totals/20);
     //$pages = 3;
 
     $newData = array();
-	for($i=1; $i<=$pages; $i++){
-		$params = "currNum=$i&rpp=20";
-		$curl -> params = $params;
-		$curl -> url = "http://vip.netxz.cn/member/index";
-		$pagesData = $curl -> getMembersPage();
-		$data = $curl ->getDownMembers($pagesData);
-		foreach ($data as $v) {
-			$newData[] = $v;
-		}
-	};
+    for($i=1; $i<=$pages; $i++){
+        $params = "currNum=$i&rpp=20";
+        $curl -> params = $params;
+        $curl -> url = "http://mry.meiruyi.vip/member/index";
+        $pagesData = $curl -> getMembersPage();
+        $data = $curl ->getDownMembers($pagesData);
+        foreach ($data as $v) {
+            $newData[] = $v;
+        }
+    };
     if($data == '') {
         //header('Location: index.php');
     }
-	$curl -> downMembersCvs($newData, $shopname);
+    $curl -> downMembersCvs($newData, $shopname);
 }else if($_GET['action'] == 'curlpackage'){
     $shopname = $_REQUEST['shopname'];
     $data = '';
@@ -66,7 +67,7 @@ if($_GET['action'] == "code"){//获取验证码
     preg_match('/共(.*)条/isU', $rs, $totals);
     $totals = isset($totals[1])?$totals[1]:100;
 
-	//总页数
+    //总页数
     $pages = ceil($totals/100);
     for($i=1; $i<=$pages; $i++){
         $params = "page.currNum=$i&page.rpp=100&set=cash&r=0.3421386775783387";
@@ -80,20 +81,20 @@ if($_GET['action'] == "code"){//获取验证码
     }
     $curl -> downPackageCvs($data, $shopname);
 }else if($_GET['action'] == 'curlstaff'){
-	$shopname = $_REQUEST['shopname'];
-	$data = '';
+    $shopname = $_REQUEST['shopname'];
+    $data = '';
 
-	//获取员工数据
-	$curl -> url = "http://vip8.sentree.com.cn/shair/employee!employeeInfo.action?set=manage&r=0.5704847458180489";
-	$rs = $curl -> curl();
+    //获取员工数据
+    $curl -> url = "http://vip8.sentree.com.cn/shair/employee!employeeInfo.action?set=manage&r=0.5704847458180489";
+    $rs = $curl -> curl();
 
-	$rsBlank = preg_replace("/\s\n\t/","",$rs);
-	//$rsBlank = str_replace(' ', '', $rsBlank);
-	preg_match_all("/table_fixed_head.*>(.*)<\/form>/isU", $rsBlank ,$tables);
+    $rsBlank = preg_replace("/\s\n\t/","",$rs);
+    //$rsBlank = str_replace(' ', '', $rsBlank);
+    preg_match_all("/table_fixed_head.*>(.*)<\/form>/isU", $rsBlank ,$tables);
 
     if(count($tables[0]) == 0) {
         header('Location: index.php');
     }
-	$curl -> downStaffCvs($tables[1][0], $shopname);
+    $curl -> downStaffCvs($tables[1][0], $shopname);
 }
 ?>
